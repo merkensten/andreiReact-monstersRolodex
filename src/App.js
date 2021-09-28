c1882d6e72d827cdcf45c85aa1e88a9352fcd3e0
Mattis <m.erkensten@icloud.com>
2021-09-28 12:54:31 +0200

    Video 39 klar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hej mitt namn är mattis</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input
+          type="search"
+          placeholder="Search for a monster..."
+          onChange= {e => this.setState({ searchField: e.target.value})}
+          
+        ></input>
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
