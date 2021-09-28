0652b1b77fe729273361b9cde47bfd5859c144fe
Mattis <m.erkensten@icloud.com>
2021-09-28 14:18:08 +0200

    Monsters Rolodex klar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -17,16 +18,25 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <input
-          type="search"
-          placeholder="Search for a monster..."
-          onChange= {e => this.setState({ searchField: e.target.value})}
-          
-        ></input>
-        <CardList monsters={this.state.monsters} />
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="Search Monsters"
+          handleChange={this.handleChange}
+        />
+
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
